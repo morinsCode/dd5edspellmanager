cb32248c7a42b8ed767ac8d585a03a17cc01d9fa
gapsofmind <morin.ibrahim@gmail.com>
2025-06-10 21:49:18 +0200

    end of the day commit

--- src/components/SpellCard.jsx
+++ src/components/SpellCard.jsx
@@ -2,10 +2,34 @@ function SpellCard({ spellDetails }) {
   return (
     <>
       <h1>Spell Card</h1>
-      <h2>{spellDetails?.name || "Select a spell to see details"}
-            
+      <h2>
+        {spellDetails?.name || "Select a spell to see details"}         
       </h2>
+      <p>Spell Level: {spellDetails?.level}</p>
+
+        {spellDetails?.damage?.damage_type?.name && (
+      <p>Damage: {spellDetails?.damage?.damage_type.name} </p>
+    )}
+
+      <p>Casting Time: {spellDetails?.casting_time}</p>
+      <p>Range: {spellDetails?.range}</p>
+      <p>Components: {spellDetails?.components?.join(" ")}</p>
+
+        {spellDetails?.concentration && (
+            <p>Concentration</p>
+        )}
+
+                {spellDetails?.ritual && (
+            <p>Ritual</p>
+        )}
+
+      <p>School: {spellDetails?.school.name}</p>
       <p>{spellDetails?.desc}</p>
+
+      {spellDetails?.higher_level[0] && (
+        <p>At higher levels: {spellDetails?.higher_level}</p>
+      )}
+      
     </>
   );
 }
