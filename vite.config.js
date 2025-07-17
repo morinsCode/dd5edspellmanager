95610349a6343b02fd35bcc440ecbd1569fe591a
gapsofmind <morin.ibrahim@gmail.com>
2025-07-17 13:57:39 +0200

    fix

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import ghPages from "vite-plugin-gh-pages";
 
-// https://vite.dev/config/
 export default defineConfig({
   base: "/ddspellmanager/",
   plugins: [react(), ghPages()],
